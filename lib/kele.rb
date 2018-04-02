d3467c90027a9af4391228577b6f231e5ea0a13a
jared w <jaredtimothywillis@gmail.com>
2018-04-02 02:24:34 -0700

    get mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,3 +1,6 @@
+require 'httparty'
+require 'json'
+
 class Kele
   include HTTParty
 
@@ -16,5 +19,11 @@ class Kele
     response = self.class.get('/users/me', headers: { "authorization" => @user_auth_token })
     JSON.parse(response.body)
   end
-    
+
+  def get_mentor_availability
+      @mentor_id = 0078
+      response = self.class.get('/mentors/0078/student_availability', headers: { "authorization" => @user_auth_token })
+      JSON.parse(response.body)
+  end
+
 end
