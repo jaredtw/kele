3dc0ad6523b0a70cdf04a68a94d39c0f992f4dcb
jared w <jaredtimothywillis@gmail.com>
2018-04-18 01:18:41 -0700

    kele messaging

--- lib/kele.rb
+++ lib/kele.rb
@@ -34,4 +34,25 @@ class Kele
       # open
   end
 
+  def get_messages(page = 0)
+      if page > 0
+          message_url = "/message_threads?page=#{page}"
+      else
+          message_url = "/message_threads"
+      end
+      response = self.class.get(message_url, headers: { "authorization" => @user_auth_token })
+      JSON.parse(response.body)
+  end
+
+  def create_message(sender, recipient_id, subject, stripped_text, token = nil)
+    response = self.class.post("/messages", headers: { "authorization" => @user_auth_token }, body: {
+        sender: sender,
+        recipient_id: recipient_id,
+        token: token,
+        subject: subject,
+        stripped_text: stripped_text
+      })
+    # puts "You're message has been sent." if response.success?
+  end
+
 end
